1a68e9474247ef3aa276f1121bc0735e2597aea3
Shubham-Yadav04 <risuyadav24@gmail.com>
2025-09-30 17:58:33 +0530

    homepage

---
+++ frontend/src/resultPage/Analysis.jsx
@@ -0,0 +1,11 @@
+
+
+function Analysis() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Analysis
---
+++ frontend/src/utils/ServiceInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ServiceInfo(props) {
+  return (
+    <div className='h-[500px] w-[350px] rounded-md relative hover:shadow-md  hover:shadow-purple hover:-translate-y-5 transition-all'>
+      <div className='w-[350px] h-[1px] absolute bottom-0 bg-gradient-to-r from-transparent via-purple-400 to-transparent '></div>
+      <img className='w-full h-[250px] object-cover' src={props.url}>
+      </img>
+      <h1 className='w-full p-2 text-2xl font-bold h-fit text-[#111] dark:text-white text-center '>{props.title}</h1>
+      <p className='w-full h-fit font-semibold p-2 text-neutral-600 '>
+        {props.para}
+      </p>
+    </div>
+  )
+}
+
+export default ServiceInfo
